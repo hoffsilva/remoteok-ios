b76cd9d0015db7d4558581fb5474a207337ad85d
Hoff Silva <hoff.henry@gmail.com>
2018-03-28 19:36:43 -0300

    First commit.

---
+++ RemoteOk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteOk
+//
+//  Created by Hoff Henry Pereira da Silva on 28/03/2018.
+//  Copyright © 2018 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
