8523d6adf097d27a8a96bc5ff99a5f1629e140da
Hoff Silva <hoff.henry@gmail.com>
2023-03-16 21:17:22 -0400

    Added custom push sound.

--- RemoteOk/AppDelegate.swift
+++ RemoteOk/AppDelegate.swift
@@ -15,13 +15,8 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
-        // [START set_messaging_delegate]
         Messaging.messaging().delegate = self
-        // [END set_messaging_delegate]
-        // Register for remote notifications. This shows a permission dialog on first run, to
-        // show the dialog at a more appropriate time move this registration accordingly.
-        // [START register_for_notifications]
-        UNUserNotificationCenter.current().delegate = self
+
         
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(
@@ -89,44 +84,44 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 }
 
 // [START ios_10_message_handling]
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    // Receive displayed notifications for iOS 10 devices.
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification) async
-    -> UNNotificationPresentationOptions {
-        let userInfo = notification.request.content.userInfo
-        
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        // [START_EXCLUDE]
-        // Print message ID.
-//        if let messageID = userInfo[gcmMessageIDKey] {
-//            print("Message ID: \(messageID)")
-//        }
-        // [END_EXCLUDE]
-        // Print full message.
-        print(userInfo)
-        
-        // Change this to your preferred presentation option
-        return [[.badge, .sound]]
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse) async {
-        let userInfo = response.notification.request.content.userInfo
-        
-        // [START_EXCLUDE]
-        // Print message ID.
-//        if let messageID = userInfo[gcmMessageIDKey] {
-//            print("Message ID: \(messageID)")
-//        }
-        // [END_EXCLUDE]
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        // Print full message.
-        print(userInfo)
-    }
-}
+//extension AppDelegate: UNUserNotificationCenterDelegate {
+//    // Receive displayed notifications for iOS 10 devices.
+//    func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                willPresent notification: UNNotification) async
+//    -> UNNotificationPresentationOptions {
+//        let userInfo = notification.request.content.userInfo
+//
+//        // With swizzling disabled you must let Messaging know about the message, for Analytics
+//        // Messaging.messaging().appDidReceiveMessage(userInfo)
+//        // [START_EXCLUDE]
+//        // Print message ID.
+////        if let messageID = userInfo[gcmMessageIDKey] {
+////            print("Message ID: \(messageID)")
+////        }
+//        // [END_EXCLUDE]
+//        // Print full message.
+//        print(userInfo)
+//
+//        // Change this to your preferred presentation option
+//        return [[.badge, .sound]]
+//    }
+//
+//    func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                didReceive response: UNNotificationResponse) async {
+//        let userInfo = response.notification.request.content.userInfo
+//
+//        // [START_EXCLUDE]
+//        // Print message ID.
+////        if let messageID = userInfo[gcmMessageIDKey] {
+////            print("Message ID: \(messageID)")
+////        }
+//        // [END_EXCLUDE]
+//        // With swizzling disabled you must let Messaging know about the message, for Analytics
+//        // Messaging.messaging().appDidReceiveMessage(userInfo)
+//        // Print full message.
+//        print(userInfo)
+//    }
+//}
 
 // [END ios_10_message_handling]
 extension AppDelegate: MessagingDelegate {
