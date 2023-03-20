5e48583c5d7f93c505fd9db6d8a741d64b396895
Hoff Silva <hoff.henry@gmail.com>
2023-03-20 18:43:09 -0400

    Fix bug fixes on push notification

--- RemoteOk/AppDelegate.swift
+++ RemoteOk/AppDelegate.swift
@@ -11,37 +11,63 @@ import SwiftUI
 import FirebaseCore
 import FirebaseMessaging
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+//
+//  Copyright (c) 2016 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
     private let persistenceManager = Container.makePersistenceManager()
     private let fcmTokenUseCase = Container.makeSaveFCMTokenUseCase()
     private let pushNotificationManager = Container.makePushNotificationManager()
     
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication
+                        .LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
         pushNotificationManager.requestAuthorization()
         application.registerForRemoteNotifications()
         return true
     }
     
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+         Messaging.messaging().apnsToken = deviceToken
+    }
 }
 
-extension AppDelegate: MessagingDelegate {
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification) async
+    -> UNNotificationPresentationOptions {
+        return [[.banner, .sound]]
+    }
+}
 
+
+extension AppDelegate: MessagingDelegate {
+    
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         guard let isPushNotificationAuthorized = persistenceManager.read(forKey: .pushNotification) as? Bool,
-        let token = fcmToken,
-        isPushNotificationAuthorized else { return }
+              let token = fcmToken,
+              isPushNotificationAuthorized else { return }
+        print(token)
         fcmTokenUseCase.saveFCMToken(token: token)
     }
-    
-    func application(application: UIApplication,
-                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      Messaging.messaging().apnsToken = deviceToken
-    }
-    
 }
 
 @main
