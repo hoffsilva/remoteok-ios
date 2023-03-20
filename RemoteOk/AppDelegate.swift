65bbc71f94adf559f7434ce6be43c703f3b68863
Hoff Silva <hoff.henry@gmail.com>
2023-03-20 14:00:36 -0400

    Added Firebase SPM

--- RemoteOk/AppDelegate.swift
+++ RemoteOk/AppDelegate.swift
@@ -8,10 +8,40 @@
 
 import UIKit
 import SwiftUI
-import CoreData
+import FirebaseCore
+import FirebaseMessaging
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    private let persistenceManager = Container.makePersistenceManager()
+    private let fcmTokenUseCase = Container.makeSaveFCMTokenUseCase()
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        application.registerForRemoteNotifications()
+        return true
+    }
+    
+}
+
+extension AppDelegate: MessagingDelegate {
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        guard let isPushNotificationAuthorized = persistenceManager.read(forKey: .pushNotification) as? Bool,
+        let token = fcmToken,
+        isPushNotificationAuthorized else { return }
+        fcmTokenUseCase.saveFCMToken(token: token)
+    }
+    
+}
 
 @main
 struct AppyApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
